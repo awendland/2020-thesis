4617831ad73d719f40550d457972ccd9781b10b8
Alex Wendland <me@alexwendland.com>
2020-03-07 11:42:48 -0500

    [WIP] demo06: Use repr(transparent) instead of repr(C)

--- demo06-zig-stack-adt-rust-consumer/src_rs/lib.rs
+++ demo06-zig-stack-adt-rust-consumer/src_rs/lib.rs
@@ -6,35 +6,35 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // https://doc.rust-lang.org/nomicon/ffi.html#representing-opaque-structs
 // #[repr(C)] pub struct Stack { loc: i32 }
-// #[repr(C)] pub struct Stack(i32);
+#[repr(transparent)] pub struct Stack(i32);
 
-// impl Drop for Stack {
-//     fn drop(&mut self) {
-//         unsafe {
-//             Stack__deinit(self);
-//         }
-//     }
-// }
+impl Drop for Stack {
+    fn drop(&mut self) {
+        unsafe {
+            Stack__deinit(self);
+        }
+    }
+}
 
-type Stack = i32;
+// type Stack = i32;
 
 extern "C" {
     // NOTE: if these are set as &mut, then Rust will export Wasm that does
     // offset operations in a manner that causes Node to be
     // `terminated by signal SIGBUS (Misaligned address error)`
-    pub fn Stack__init(inst: Stack);
-    pub fn Stack__deinit(inst: Stack);
-    pub fn Stack__push(inst: Stack, item: i32);
-    pub fn Stack__pop(inst: Stack) -> i32;
+    pub fn Stack__init(inst: &mut Stack);
+    pub fn Stack__deinit(inst: &mut Stack);
+    pub fn Stack__push(inst: &mut Stack, item: i32);
+    pub fn Stack__pop(inst: &mut Stack) -> i32;
 }
 
 #[no_mangle]
 pub extern fn use_stack_adt(val: i32) -> i32 {
-    let s1: Stack = 1;
+    let mut s1: Stack = Stack(1);
     unsafe {
-        Stack__init(s1);
-        Stack__push(s1, val);
-        return Stack__pop(s1);
+        Stack__init(&mut s1);
+        Stack__push(&mut s1, val);
+        return Stack__pop(&mut s1);
     }
 }
 
